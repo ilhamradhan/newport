a74bf441c2340659f3c468265e1c30fb4ce7d54e
ilhamradhan <ramadhan@fh-brandenburg.de>
2023-01-17 20:04:17 +0100

    Updates

---
+++ precache-manifest.cc10b93238bd38f5c09e091a736507e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3d32fe41b92d6a396c868eaba5ffb2",
+    "url": "/newport/index.html"
+  },
+  {
+    "revision": "6b6e94ed0ec29467a3fb",
+    "url": "/newport/static/css/main.30a041e4.chunk.css"
+  },
+  {
+    "revision": "0c357c2a0bacecd26f43",
+    "url": "/newport/static/js/2.d2977431.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/newport/static/js/2.d2977431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6e94ed0ec29467a3fb",
+    "url": "/newport/static/js/main.922ec583.chunk.js"
+  },
+  {
+    "revision": "6375b5f59dae05e57200",
+    "url": "/newport/static/js/runtime-main.b2533885.js"
+  },
+  {
+    "revision": "562b6d8fb1758281af1653a0fc77289b",
+    "url": "/newport/static/media/ilham.562b6d8f.jpg"
+  },
+  {
+    "revision": "13352d3fe91142c19e000ed42dca16c3",
+    "url": "/newport/static/media/powerbidashboard1.13352d3f.png"
+  }
+]);
